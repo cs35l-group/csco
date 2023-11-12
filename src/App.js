0915d08ca40fe47ba9defd5f98f77c8792b99c60
Jayson Tian <48577877+Jaysontian@users.noreply.github.com>
2023-11-11 16:29:48 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          CSCO
+        </h1>
+        <p>A collective board to organize your media.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
