e7bc11e96b325580b2dad778574ce67c0efc9bb4
Will Steinberg <wbensteinberg@gmail.com>
2023-11-13 19:54:08 -0800

    will test 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          CSCO Will
+          CSCO Will 2
         </h1>
         <p>A collective board to organize your media.</p>
       </header>
