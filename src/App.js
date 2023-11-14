475e95a89300bbcdc2dad11de08c88de9db7aafd
soysauceshark <67615657+soysauceshark@users.noreply.github.com>
2023-11-13 21:12:42 -0800

    ULTIMATE SHARLENE CHANGE

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          CSCO Will 2
+          CSCO
         </h1>
         <p>A collective board to organize your media.</p>
       </header>
