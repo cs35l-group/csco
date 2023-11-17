461e32a911bf378f4b0bed78e3171f0215f629b9
Rishabh Sharma <rishabhsharma7676@gmail.com>
2023-11-17 13:45:41 -0800

    testing gitignore
    trying to see if node nodules gets ignored

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Login.css'; 
 
 function Home() {
   return (
       <div>
-        <p>This is the home page</p>
+        <p>This is the Home page</p>
         <Link to="/">
             <button type="button">Go to Login</button>
         </Link>
