0cb12175c5dc8010eae2127909fca5f192ad956f
Rishabh Sharma <rishabhsharma7676@gmail.com>
2023-11-16 19:49:57 -0800

    added call to backend to official login page
    now when username and password are entered into fields, it registers an entry into the database

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react';
 import React from 'react';
 import './Login.css'; 
 
 function Login() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:4000/api/logins', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+    .then(response => response.json())
+    .then(data => console.log('User signed up!:', data))
+    .catch(error => console.error('Error signing up:', error));
+  };
+
+  // return (
+  //   <form id="Sign up form" onSubmit={handleSubmit}>
+  //     <input
+  //       type="text"
+  //       value={username}
+  //       onChange={e => setUsername(e.target.value)}
+  //       placeholder="Username"
+  //     />
+  //     <input
+  //       type="text"
+  //       value={password}
+  //       onChange={e => setPassword(e.target.value)}
+  //       placeholder="Password"
+  //     />
+  //     <button type="submit">Add Photo</button>
+  //   </form>
+  // );
   return (
     <div className="Login-top">
       <header className="login-header">
@@ -11,18 +47,30 @@ function Login() {
         <p>Log into your account</p>
       </header>
       <div className='Login-section'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-row">
             {/* <label htmlFor="email">Email: </label> */}
-            <input type="email" id="email" name="email" placeholder="Email" />
+            <input
+              type="text"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              placeholder="Username"
+            />
+            {/* <input type="email" id="email" name="email" placeholder="Email" /> */}
           </div>
 
           <div className="form-row">
             {/* <label htmlFor="password">Password: </label> */}
-            <input type="password" id="password" name="password" placeholder="Password"/>
+            <input
+              type="text"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+            {/* <input type="password" id="password" name="password" placeholder="Password"/> */}
             </div>
+            <button className="LoginButton" type="submit">Login</button>
         </form> 
-        <button className="LoginButton" type="submit">Login</button>
 
       </div>
     </div>
