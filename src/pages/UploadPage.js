202e122464a116191ec7d70b6a96394c40729e06
soysauceshark <67615657+soysauceshark@users.noreply.github.com>
2023-11-29 00:40:27 -0800

    Styled upload page
    used Jayson's Poppins font, has gray background right now

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -2,25 +2,44 @@ import React, { useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import './UploadPage.css';
+import Header from '../components/header';
 
 const FileUpload = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const { getRootProps, getInputProps } = useDropzone({
     onDrop: (acceptedFiles) => {
       setUploadedFiles(acceptedFiles);
-      // Call backend API to upload files, need to figure this out
     },
   });
+
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <p>Drag and drop files you want to upload.</p>
-      <ul>
-        {uploadedFiles.map((file) => (
-          <li key={file.name}>{file.name}</li>
-        ))}
-      </ul>
+    <>
+    <div>
+      <Header />
     </div>
+    <div className="upload-container">
+        <div className="upload-header">
+          <h2>Create a Post</h2>
+        </div>
+        <div {...getRootProps()} className="upload-dropzone">
+          <input {...getInputProps()} />
+          <p>Drag and drop your image here</p>
+        </div>
+        <div className="uploaded-files">
+          <h3>Uploaded Files</h3>
+          <ul>
+            {uploadedFiles.map((file) => (
+              <li key={file.name}>{file.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="upload-actions">
+          <button className="upload-button">Create Post</button>
+        </div>
+      </div>
+    </>
   );
 };
+
 export default FileUpload;
